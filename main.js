3f87c30e2b9e71088ea50de686c18ebc25cbc632
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-03-07 22:07:25 +0300

    Finalized the javascript function to display the akan name, modified the results output in html and css

--- main.js
+++ main.js
@@ -1,32 +1,40 @@
 'use strict';
 
+const akanize = document.getElementById("submit");
+
 const getAkanName = function() {
   const maleAkan = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "yaw", "kofi", "kwame"];
   const femaleAkan =["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
   const gender = document.querySelector('input[type="radio"]:checked');
   const date = document.getElementById('date');
-  const akan = document.getElementsByClassName('akan-name');
-  const results = document.getElementById("results");
+  const akan = document.getElementById('akan');
+  const hooray = document.getElementById("hooray");
 
   const dateOfBirth = date.value;
   const birthDate = new Date(dateOfBirth);
   let day = birthDate.getDay();
 
-  if(day === 1){
-    console.log("Monday");
+ 
+  
+  if(gender.value === "Male"){
+    hooray.classList.remove('hidden');
+    akan.innerHTML = maleAkan[day];
   }
+ 
 
-  console.log(day);
-  console.log(gender.value);
+ if(gender.value === "Female"){
+   hooray.classList.remove('hidden');
+   akan.innerHTML = femaleAkan[day];
+ }
 
-  if(gender === "Male") {
-    document.getElementById('akan-name').innerHTML = maleAkan[day];
-  // }
-  // gender.value === "Male" ? akan.innerHTML = maleAkan[day] : alert('Select gender');
-  // gender.value === "Female" ? akan.innerHTML = femaleAkan[day] : alert('Select gender');
-  }
+
+  // gender.value === "Male" ? akan.innerHTML = maleAkan[day] : gender.value === "Female" ? akan.innerHTML = femaleAkan[day] : alert('Select gender');
+
+  date.reset();  // Reset
+   return true;
 
 }
-const akanize = document.getElementById("submit");
+
 
 akanize.addEventListener("click", getAkanName);
